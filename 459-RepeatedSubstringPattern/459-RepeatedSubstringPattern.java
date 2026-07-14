be669439dcbd8e11393f61ed6721fdd2142bf720
Vishal Kumar Vats <vishalvats0411@gmail.com>
2026-07-14 14:52:18 +0530

    File updated at 14/07/2026, 14:52:16

--- 459-RepeatedSubstringPattern/459-RepeatedSubstringPattern.java
+++ 459-RepeatedSubstringPattern/459-RepeatedSubstringPattern.java
@@ -1,24 +1,9 @@
-// Last updated: 14/07/2026, 14:15:22
+// Last updated: 14/07/2026, 14:52:16
 1class Solution {
-2    public boolean repeatedSubstringPattern(String s) {
+2    public boolean rotateString(String s, String goal) {
 3        int n=s.length();
-4        for(int i=n/2;i>0;i--)
-5        {   
-6            if(n%i==0)
-7            {
-8                StringBuilder compare=new StringBuilder();
-9                int appendtimes=n/i;
-10                String pattern=s.substring(0,i);
-11                for(int j=1;j<=appendtimes;j++)
-12                {
-13                    compare.append(pattern);
-14                }
-15                if(s.equals(compare.toString()))
-16                {
-17                    return true;
-18                }
-19            }
-20        }
-21        return false;
-22    }
-23}
+4        if(n!=goal.length())
+5            return false;
+6        return (s+s).contains(goal);
+7    }
+8}
